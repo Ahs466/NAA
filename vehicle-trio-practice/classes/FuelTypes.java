cd68f94c37552c2b7b77504407a85f714c674d6d
anika <anikahussain1@outlook.com>
2024-10-29 14:19:52 +0000

    EDIT: motorisedvehicles etc

---
+++ vehicle-trio-practice/classes/FuelTypes.java
@@ -0,0 +1,5 @@
+public enum FuelTypes{
+    PETROL,
+    DIESEL,
+    ELECTIC,
+}
---
+++ vehicle-trio-practice/classes/MotorisedVehicle.java
@@ -0,0 +1,14 @@
+import classes.FuelTypes;
+
+public class MotorisedVehicle extends Vehicle{
+
+    private int mileage;
+    private String lastService;
+
+    private String lastMOT;
+
+    private int fuelCapacity;
+
+    private FuelTypes fuel;
+
+}
